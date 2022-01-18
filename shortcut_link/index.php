d14a9b84bdf695358a504d94200df26694815beb
Jakub Wojdziak <kubaxplay@o2.pl>
2022-01-18 11:06:24 +0100

    Added better security

--- shortcut_link/index.php
+++ shortcut_link/index.php
@@ -1,67 +1,115 @@
 <?php
-include('db.php');
-$local = $_SERVER['HTTP_REFERER'];
-$url = (isset($_SERVER['QUERY_STRING']) && !empty($_SERVER['QUERY_STRING'])) ? $_SERVER['QUERY_STRING'] : '';
-if ($url !== ''){
-    $url_explode = explode('/', $url);
-
-    $short_exist = $db->query("SELECT link FROM links WHERE short='$url_explode[0]'");
-        while ($row = $short_exist->fetch_assoc()) {
-            if (!empty($row['link'])) {
-                header('Location: ' . $row['link']);
-                die();
-            }
-        }
+include("db.php");
+$url = isset($_SERVER["QUERY_STRING"]) && !empty($_SERVER["QUERY_STRING"]) ? $_SERVER["QUERY_STRING"] : "";
+if ($url !== "") {
+  $url_explode = explode("/", $url);
+  if (preg_match("/^[A-Za-z0-9]+$/i", $url_explode[0]) === 1) {
+    $short_exist = $db->prepare("SELECT link FROM links WHERE short=:short");
+    $short_exist->execute(['short' => $url_explode[0]]);
+    while ($row = $short_exist->fetch()) {
+      if (!empty($row["link"])) {
+        header("Location: " . $row["link"]);
+        $db = null;
+        die();
+      }
+    }
+  }
 }
+$db = null;
 ?>
 
 <!DOCTYPE html>
 <html>
 
 <head>
-    <meta charset="utf-8">
-    <title>Short your link</title>
-    <script src="js/jquery.js"></script>
-    <script src="js/jquery-validate.js"></script>
+  <meta charset="UTF-8">
+  <title>Skróć swój link</title>
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="shortcut icon" href="./img/svg/logo.svg" type="image/x-icon">
+  <link rel="stylesheet" href="./css/style.min.css">
+  <script src="js/jquery.js"></script>
+  <script src="js/jquery-validate.js"></script>
 </head>
 
 <body>
-    <div class="container">
-        <form method="POST" name="short_link">
-            <label for="url">Wprowadź link do skrócenia</label>
-            <input type="text" name="url" id="url">
-            <button type="submit">Skróć</button>
-        </form>
-        <a id="output"></a>
+  <div class="layer"></div>
+  <a class="skip-link sr-only" href="#skip-target">Skip to content</a>
+  <div class="page-flex">
+    <div class="main-wrapper">
+      <nav class="main-nav--bg">
+        <div class="container main-nav">
+          <div class="main-nav-end">
+          </div>
+        </div>
+      </nav>
+      <main class="main users chart-page" id="skip-target">
+        <div class="container">
+          <h2 class="main-title">Skróć link</h2>
+          <div class="row stat-cards">
+            <div class="col-md-6">
+              <form method="POST" class="form" name="short_link">
+                <label class="form-label-wrapper">
+                  <p class="form-label">Wprowadź link do skrócenia</p>
+                  <input type="text" class="form-input" name="url" id="url">
+                </label>
+                <button type="submit" class="form-btn primary-default-btn transparent-btn">Skróć</button>
+              </form>
+            </div>
+            <div class="col-md-6">
+              <div id="link-output">
+                <a id="output" class="link-info"></a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
+      <footer class="footer">
+        <div class="container footer--flex">
+          <div class="footer-start">
+            <p><?php echo Date("Y"); ?> © Wojak</p>
+          </div>
+        </div>
+      </footer>
     </div>
-    <script>
-        $(document).ready(function() {
-            $.validator.addMethod('validUrl', function(value, element) {
-                var url = $.validator.methods.url.bind(this);
-                return url(value, element) || url('http://' + value, element);
-            }, 'Please enter a valid URL');
+  </div>
+  <!-- Icons library -->
+  <script src="plugins/feather.min.js"></script>
+  <script>
+    $(document).ready(function() {
+      $.validator.addMethod("validUrl", function(value, element) {
+        var url = $.validator.methods.url.bind(this);
+        return url(value, element) || url("http://" + value, element);
+      }, "Wpisz prawidłowy link");
 
-            $("form[name='short_link']").validate({
-                rules: {
-                    url: {
-                        required: true,
-                        validUrl: true
-                    }
-                },
-                messages: {
-                    url: 'Please enter a valid URL'
-                },
-                submitHandler: function(form) {
-                    console.log(form['url'].value);
-                    $.post('short.php', {data: form['url'].value}, function(result) {
-                        console.log(result);
-                        $('#output').attr('href','<?php echo $local;?>'+result['short']);
-                        $('#output').append('<?php echo $local;?>'+result['short']);
-                    }, 'json');
-                }
-            });
-        });
-    </script>
+      $("form[name='short_link']").validate({
+        rules: {
+          url: {
+            required: true,
+            validUrl: true
+          }
+        },
+        messages: {
+          url: "Wpisz prawidłowy link"
+        },
+        submitHandler: function(form) {
+          $.post('short.php', {
+            link: form["url"].value,
+          }, function(result) {
+            if (result.ERROR) {
+              $("#output").attr("href", "/");
+              $("#link-output").attr("style", "margin-top:1rem;text-align:center;background-color:#fff;border-radius:10px;padding:40px 40px 56px;");
+              $("#output").text("Podaj prawidłowy link!");
+            } else {
+              $("#output").attr("href", result["short"]);
+              $("#link-output").attr("style", "margin-top:1rem;text-align:center;background-color:#fff;border-radius:10px;padding:40px 40px 56px;");
+              $("#output").text("<?php echo $_SERVER['SERVER_NAME']; ?>/" + result["short"]);
+            }
+          }, "json");
+        }
+      });
+    });
+  </script>
 </body>
 
 </html>
--- shortcut_link/short.php
+++ shortcut_link/short.php
@@ -1,11 +1,21 @@
 <?php
 include('db.php');
 
-$link = (isset($_POST['data']) && !empty($_POST['data'])) ? $_POST['data'] : '';
+$link = (isset($_POST['link'])) && !empty($_POST['link']) ? $_POST['link'] : '';
+$linkParsed = parse_url($link);
+if (!empty($linkParsed['scheme'])) {
+    if ("http" !== $linkParsed['scheme'] && "https" !== $linkParsed['scheme']) {
+        echo json_encode(['ERROR' => true]);
+        $db = null;
+        die();
+    }
+} else {
+    $link = 'http://' . $link;
+}
+
 $loop = 1;
 
-function generateRandomString($length = 10)
-{
+function generateRandomString($length = 10) {
     $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
     $charactersLength = strlen($characters);
     $randomString = '';
@@ -15,20 +25,23 @@ function generateRandomString($length = 10)
     return $randomString;
 }
 
-while($loop == 1){
-    $link_exist = $db->query("SELECT link FROM links WHERE link='$link'");
-    if (!empty($link_exist->fetch_assoc())) {
-        $temp = $db->query("SELECT * FROM links WHERE link='$link'");
-        $result = $temp->fetch_assoc();
+while ($loop === 1) {
+    $link_exist = $db->prepare("SELECT link FROM links WHERE link=:link");
+    $link_exist->execute(['link' => $link]);
+    if (!empty($link_exist->fetch())) {
+        $temp = $db->prepare("SELECT short FROM links WHERE link=:link");
+        $temp->execute(['link' => $link]);
+        $result = $temp->fetch();
         echo json_encode($result);
         $loop = 0;
     } else {
-        $short = generateRandomString();
-        $short_exist = $db->query("SELECT short FROM links WHERE short='$short'");
-        if (empty($short_exist->fetch_assoc())) {
-            $db->query("INSERT INTO links (link, short) VALUES ('$link', '$short')");
+        $short = generateRandomString(random_int(5, 10));
+        $short_exist = $db->prepare("SELECT short FROM links WHERE short=:short");
+        $short_exist->execute(['short' => $short]);
+        if (empty($short_exist->fetch())) {
+            $db->prepare("INSERT INTO links (link, short) VALUES (:link, :short)")->execute(['link' => $link, 'short' => $short]);
         }
     }
 }
 
-$db->close();
+$db = null;
